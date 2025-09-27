c1276dc630664dd5029e654b3bcd7f9d9b2bffb8
KerYagciHTL <k.yagci@students.htl-leonding.ac.at>
2025-09-27 18:14:02 +0200

    changed ci.yml

--- vendor/prepare.sh
+++ vendor/prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
+GLFW_TAG="${GLFW_TAG:-3.3-stable}"
 if [ -d "glfw" ]; then
   echo "glfw directory already exists, skipping clone."
   exit 0
 fi
-
-git clone --depth 1 --branch 3.3-stable https://github.com/glfw/glfw.git glfw
+git clone --depth 1 --branch "$GLFW_TAG" https://github.com/glfw/glfw.git glfw
